107c3d16412aa8d94e84cd1247cc89e635fdfd99
kingtamaaj <kingtamaaj@gmail.com>
2019-05-13 19:08:58 -0400

    folder and components setup

---
+++ instagram/instagram-app/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import CommentSection from './Components/CommentSection';
+import PostContainer from './Components/PostContainer';
+import SearchBar from './Components/SearchBar'; 
+
+
+class App extends React.Component 
+  {
+    constructor()
+    {
+      super();
+      this.state = {
+
+      };
+    }
+
+    render() 
+    {
+      <div className="App"> 
+        
+      
+      </div>
+    }
+}
+
+
+export default App;
---
+++ instagram/instagram-app/src/Components/SearchBar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './SearchBar.css'
+
+
+
+
+export default SearchBar;
